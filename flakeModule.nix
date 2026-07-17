a9dd611d31a55ca1ab089022396902e343be4235
Ernesto Rodriguez <connect@netowork.me>
2026-07-17 23:39:13 +0100

    Define the 'pkgs' overlay on top of the prelude naked library.

--- flakeModule.nix
+++ flakeModule.nix
@@ -7,27 +7,43 @@ let
     mkOption
     types;
   config' = config;
-  tikal-config = {
+  tikal-config-flake = {
+    log-level = config.tikal.log-level;
+    test-filters = config.tikal.test-filters;
+  };
+  tikal-config-debug = {
     log-level = 8;
     test-filters = [ { glob = "*"; } ];
   };
+  glob-type = types.submodule {
+    options = {
+      glob = lib.mkOption {
+        type = types.str;
+        description = "The glob expression as a string";
+      };
+    };
+  };
+  make-overlay = { tikal-config }: final: prev:
+  let
+    inherit (prev) lib;
+    prelude = prev.callPackage ./prelude.nix { inherit tikal-config nixpkgs; };
+  in
+    {
+      tikal.prelude = prelude;
+    }
+  ;
+  overlay = lib.makeOverridable make-overlay { tikal-config = tikal-config-flake; };
 in
 {
   config = {
-    flake.overlays.default = final: prev:
-    let
-      inherit (prev) lib;
-      prelude = lib.makeScope prev.newScope (import ./prelude.nix);
-    in
-      {
-        tikal.prelude = prelude;
-      }
-    ;
+    flake.overlays.default = overlay;
+    flake.overlays.debug = overlay.override { tikal-config = tikal-config-debug; };
     flake.lib =
     let
       lib-scope =
         lib.makeScope lib.callPackagesWith (self: {
-          inherit lib nixpkgs tikal-config;
+          inherit lib nixpkgs;
+          tikal-config = tikal-config-flake;
           prelude = self.callPackage ./prelude-lib.nix {};
         });
     in
@@ -35,16 +51,49 @@ in
     ;
   };
   options = {
+    tikal = {
+      log-level = lib.mkOption {
+        description = ''
+          The log verbosity of the output when evaluating the tikal code. Normally,
+          this should be left unchanged but can be used when modifying the tikal
+          code for debugging. The loglevels are as follows:
+            * debug-verbose = 8
+            * debug = 7
+            * info = 6
+            * warning = 5
+            * error = 4
+            * no logs = < 4
+        '';
+        type = types.enum [ 0 1 2 3 4 5 6 7 8 ];
+        default = 0;
+      };
+      test-filters = lib.mkOption {
+        description = ''
+          The tests for Tikal can run automatically as tikal code gets evaluated.
+          This allows developers to easily asess if new changes break any tests.
+          However, this can incurr a performance penalty, therefore a filter can
+          be used to determine what tests are to be run.
+        '';
+        default = [];
+        type = types.listOf (types.oneOf [ types.str glob-type ]);
+      };
+    };
 
     perSystem = mkPerSystemOption ({ pkgs, system, config, ... }:
     let
-      inherit (pkgs.extend self.overlays.default) tikal;
+      inherit (pkgs.extend self.overlays.debug) tikal;
     in
       {
         checks.tikal-prelude = tikal.prelude.checks.all;
-        checks.tiakl-prelude-lib = self.lib.log.log-info "success" (
-          pkgs.writeText "test" "test"
-        );
+
+        # simple check to ensure the lib is well defined
+        # All of the lib's functionality is checked by the
+        # overlay test.
+        checks.tikal-prelude-lib =
+          self.lib.log.log-info "The lib is well defined" (
+            pkgs.writeText "The lib is well defined" ""
+          )
+        ;
       }
     );
   };
