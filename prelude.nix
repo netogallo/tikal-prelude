9bc5acc224bbc44fb3a6f4a2d25c51029e64c001
Ernesto Rodriguez <connect@netowork.me>
2026-07-18 13:49:06 +0100

    WIP: more patches.

--- prelude.nix
+++ prelude.nix
@@ -6,6 +6,4 @@ in
     checks = super.callPackage ./checks.nix {};
     xonsh = super.callPackage ./prelude/xonsh.nix {};
     template = super.callPackage ./prelude/template.nix {};
-    inherit (self.trace-lib) trace trace-value debug-print;
-    inherit (self.do-lib) do;
   })
