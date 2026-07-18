63db5392b1f970c306e9a57bd7978a12dafcadef
Ernesto Rodriguez <connect@netowork.me>
2026-07-18 14:03:56 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
         # those are more easily expressed in perSystem.
-        flakeModules.default = flake-parts-lib.importApply ./flakeModules.nix {};
+        flakeModules.default = flake-parts-lib.importApply ./flakeModule.nix {};
       };
     });
 }
